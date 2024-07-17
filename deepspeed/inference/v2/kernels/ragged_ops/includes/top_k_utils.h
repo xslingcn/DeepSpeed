007fe4cb2b81c531e54d006b9a674f25cbd022b8
xsling <dev@xsl.ing>
2024-07-17 01:33:31 +0000

    Add support for Qwen2Moe (A14B)
    Ref: https://github.com/microsoft/DeepSpeed/pull/5403

--- deepspeed/inference/v2/kernels/ragged_ops/includes/top_k_utils.h
+++ deepspeed/inference/v2/kernels/ragged_ops/includes/top_k_utils.h
@@ -11,5 +11,8 @@
         } else if (2 == N_TOP_K) {         \
             constexpr int CONST_TOP_K = 2; \
             __VA_ARGS__();                 \
+        } else if (8 == N_TOP_K) {         \
+            constexpr int CONST_TOP_K = 8; \
+            __VA_ARGS__();                 \
         }                                  \
     }()
